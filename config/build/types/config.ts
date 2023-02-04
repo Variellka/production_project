cef65a2623a2a51d8838273114ef38a224ceeab5
Xenia Anichkina <xenia.anichkina@oracle.com>
2023-02-04 21:32:28 +0300

    added config

---
+++ config/build/types/config.ts
@@ -0,0 +1,21 @@
+import webpack from "webpack";
+
+//webpack.Configuration["mode"] - "none" | "development" | "production";
+
+export type BuildPaths = {
+  entry: string;
+  build: string;
+  html: string;
+};
+
+export interface BuildOptions {
+  mode: webpack.Configuration["mode"];
+  paths: BuildPaths;
+  isDev: boolean;
+  port: number;
+}
+
+export interface BuildEnv {
+  mode: webpack.Configuration["mode"];
+  port: number;
+}
