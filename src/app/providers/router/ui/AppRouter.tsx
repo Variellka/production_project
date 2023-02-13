d2a77b286b5f946ba18b404d0ae21cef564e2dbc
Xenia Anichkina <xenia.anichkina@oracle.com>
2023-02-13 21:54:54 +0300

    20. loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,13 +1,18 @@
 import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 const AppRouter = () => (
     <Routes>
         {Object.values(routeConfig).map(({ element, path }) => (
             <Route
                 key={path}
-                element={<div className="page-wrapper">{element}</div>}
+                element={
+                <Suspense fallback={<PageLoader/>}>
+                    <div className="page-wrapper">{element}</div>
+                </Suspense>
+                }
                 path={path}
             />
         ))}
