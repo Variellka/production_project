8bf42986015c5b9e8fabcf9421940d617ac41019
Xenia Anichkina <xenia.anichkina@oracle.com>
2023-04-04 21:31:34 +0300

    28.  fixed sideBar tests

--- src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
@@ -1,13 +1,14 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
+import { renderWithRouter } from 'shared/config/tests/renderWithRouter/renderWithRouter';
 import SideBar from './SideBar';
 
 describe('SideBar tests', () => {
     test('Test render', () => {
-        render(<SideBar />);
-        expect(screen.getByText('>')).toBeInTheDocument();
+        renderWithRouter(<SideBar />);
+        expect(screen.getByText('<')).toBeInTheDocument();
     });
     test('Test render', () => {
-        render(<SideBar />);
+        renderWithRouter(<SideBar />);
         expect(screen.getByTestId('sideBar-wrapper')).toBeInTheDocument();
         fireEvent.click(screen.getByTestId('sideBar-toggle'));
         expect(screen.getByTestId('sideBar-wrapper')).toHaveClass('collapsed');
