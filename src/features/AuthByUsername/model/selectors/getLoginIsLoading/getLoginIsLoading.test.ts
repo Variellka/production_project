2e5f4f45d93994db8e368554c7f377a62a0cb37c
Xenia Levchenko <variellka@gmail.com>
2023-06-02 15:09:34 +0300

    36. tests for loginForm selectors

---
+++ src/features/AuthByUsername/model/selectors/getLoginIsLoading/getLoginIsLoading.test.ts
@@ -0,0 +1,19 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginIsLoading } from './getLoginIsLoading';
+
+describe('getLoginIsLoading.test', () => {
+    test('', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                isLoading: true,
+            },
+        };
+        expect(getLoginIsLoading(state as StateSchema)).toBe(true);
+    });
+
+    test('', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginIsLoading(state as StateSchema)).toBe(false);
+    });
+});
---
+++ src/features/AuthByUsername/model/selectors/getLoginPassword/getLoginPassword.test.ts
@@ -0,0 +1,19 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginPassword } from './getLoginPassword';
+
+describe('getLoginPassword.test', () => {
+    test('', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                password: '123',
+            },
+        };
+        expect(getLoginPassword(state as StateSchema)).toBe('123');
+    });
+
+    test('', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginPassword(state as StateSchema)).toBe('');
+    });
+});
