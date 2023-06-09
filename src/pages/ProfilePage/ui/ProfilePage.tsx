26996c9f60f9349e1c31b30b881c7590bbf95506
Xenia Levchenko <variellka@gmail.com>
2023-06-09 23:34:30 +0300

    40. loki fixes

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,6 +1,5 @@
 import { ProfileCard, fetchProfileData, profileReducer } from 'entities/Profile';
 import { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
 import DynamicModuleLoader, { ReducerList }
     from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
@@ -11,7 +10,6 @@ const initialReducers: ReducerList = {
 
 const ProfilePage = () => {
     const dispatch = useAppDispatch();
-    const { t } = useTranslation('profile');
 
     useEffect(() => {
         dispatch(fetchProfileData());
