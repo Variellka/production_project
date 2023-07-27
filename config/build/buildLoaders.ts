997ecd41c0f2a7bc04a86d2baee5980b1eacad16
Xenia Levchenko <variellka@gmail.com>
2023-07-27 17:58:57 +0300

    46. react refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,7 @@
 import type webpack from 'webpack';
 import { type BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/buildCssLoader';
+import { buildbabelLoader } from './loaders/buildbabelLoader';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     const typescriptLoader = {
@@ -14,16 +15,7 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     };
 
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-            },
-        },
-    };
+    const babelLoader = buildbabelLoader(options.isDev);
 
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff2|woff)$/i,
